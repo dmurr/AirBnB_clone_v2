aa54c6072a1eff373387eb5702a652a74d5b9e4f
Martin Smith <msmith3dart@icloud.com>
2019-04-03 23:35:03 +0000

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,5 +70,8 @@ class FileStorage:
 
         '''
         if obj:
+            try:
                 key = '{}.{}'.format(obj.__cls__.__name__, obj.id)
                 self.__objects.pop(key)
+            except:
+                pass
