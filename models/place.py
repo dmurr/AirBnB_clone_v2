81bbc5cb562bc24393023566c6c2bebdf9bcd211
Martin Smith <msmith3dart@icloud.com>
2019-04-03 23:28:34 +0000

    updated Review
    add approriate relationship to models User and Place as well

--- models/place.py
+++ models/place.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
+from models.review import Review
 from sqlalchemy import Column, String, Float, Integer, ForeignKey
-
+from os import getenv
+import models
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -23,7 +25,7 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
@@ -31,3 +33,11 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     amenity_ids = []
+
+    if getenv('HBNB_TYPE_STORAGE') is 'db':
+        reviews = relationship('Review', backref='place', cascade='all, delete')
+    else:
+        @property
+        def reviews(self):
+            objs = models.storage.all(Review)
+            return [v for k, v in obj.items() if v.place_id == self.id ]
