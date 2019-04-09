2c59ee91f3dab458e88f3159e4f4c24269900b3e
martin smith <msmith3d@icloud.com>
2019-04-08 21:46:53 -0700

    add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script will pack a directory into a tar file
+"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """This method will pack the web_static dir into a tar.gz
+    for deployinment to remote servers.
+
+    Addtionally this methos will stash all archive into a directory
+    call 'versions'
+    """
+    if not os.path.exists(os.path.dirname("./web_static")):
+        return None
+
+    if not os.path.exists(os.path.dirname("versions")):
+        try:
+            local("mkdir -p versions")
+        except Exception as e:
+            print(e)
+            return None
+    file_name = "web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+    local("echo {}".format(file_name))
+    local("tar cpfz {} ./web_static".format(file_name))
+    local("mv {f} versions/{f}".format(f=file_name))
+    return os.path.abspath("version/{}".format(file_name))
